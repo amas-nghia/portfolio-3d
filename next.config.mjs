f78ff1c812ea1cf754dcbc1c277264cdf1401c10
Nguyễn Thành Nghĩa <amasnghia@gmail.com>
2025-08-01 13:43:49 +0700

    Add GitHub Actions workflow for deploying Next.js to GitHub Pages and update next.config.mjs for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export', // Enable static export
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? '/portfolio-3d/': '',
+  basePath: isProd ? '/portfolio-3d' : '',
 }
 
 export default nextConfig
