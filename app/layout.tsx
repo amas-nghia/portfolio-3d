e347fbc8ffcd67f5ef2f8aa829dc0fe1f03e70c6
Nguyễn Thành Nghĩa <amasnghia@gmail.com>
2025-08-01 13:50:42 +0700

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Amas Portfolio 3D",
+  description: "Amas Portfolio 3D - A showcase of my work in a 3D environment",
+  openGraph: {
+    title: "Amas Portfolio 3D",
+    description:
+      "Amas Portfolio 3D - A showcase of my work in a 3D environment",
+    url: "https://amas-nghia.github.io/portfolio-3d/",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -27,5 +32,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
